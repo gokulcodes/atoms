a387d2ed0b15f7fd2f01e753bc52335b4e29539b
gokulcodes <gokulvaradan2202@gmail.com>
2025-04-06 13:35:34 +0530

    Controllers segregated

--- src/controller/atomContext.js
+++ src/controller/atomContext.js
@@ -1,4 +1,20 @@
 import { createContext } from "react";
+import { allComponents } from "./config";
 
-const atomContext = createContext(null)
-export default atomContext
+export const initialState = {
+  theme: "light",
+  fitToScreen: false,
+  components: allComponents,
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "theme":
+      return { ...state, theme: action.payload }; // don't directly mutate the state, return a new object
+    case "fitScreen":
+      return { ...state, fitToScreen: !state.fitToScreen };
+  }
+};
+
+const atomContext = createContext(null);
+export default atomContext;
--- src/controller/config.js
+++ src/controller/config.js
@@ -1,1 +1,58 @@
-export const GITHUB_LINK = 'https://github.com/gokulcodes/atoms'
+export const GITHUB_LINK = "https://github.com/gokulcodes/atoms";
+export const allComponents = [
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+  {
+    name: "AutoComplete",
+    lastCommit: "2023-10-01",
+    previewImage: "https://dummyimage.com/16:9x1080",
+  },
+];
