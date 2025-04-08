f1bae959a5bba254426c8b011cd6921ab038f0b7
gokulcodes <gokulvaradan2202@gmail.com>
2025-04-08 10:03:31 +0530

    push for git shell-script

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git add .
 message=$2
-git commit -m message
+git commit -m "$message"
 branchName=$4
-echo branchName
-git push origin branchName
+echo $branchName
+git push origin $branchName
